495a42efc945b7245ab892417c50ae719707d812
Pavan <psp.pavan.sai@gmail.com>
2023-12-01 13:52:00 +0530

    all primes less than n

--- Prime Number.py
+++ Prime Number.py
@@ -1,7 +1,9 @@
-num = 10
+num = int(input("Enter an integer"))
 cnt=0
-for i in range(1,num+1):
-    if(num%i==0):
-        cnt+=1;
-if(cnt==2):print("Prime Number")
-else:print("Not a prime number")
+for i in range(num):
+    for j in range(1,num+1):
+        if(i%j==0):
+            cnt+=1;
+    if(cnt==2):print(f"{i} is Prime Number")
+    else:print(f"{i} is not a prime number")
+    cnt=0;
