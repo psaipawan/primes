b798942977ffc48980b5b3a32b91480bd114ead2
Pavan <psp.pavan.sai@gmail.com>
2023-12-01 13:54:41 +0530

    using functions

--- Prime Number.py
+++ Prime Number.py
@@ -1,9 +1,13 @@
-num = int(input("Enter an integer"))
-cnt=0
-for i in range(num):
+def checkPrime(num):
+    cnt=0
     for j in range(1,num+1):
         if(i%j==0):
             cnt+=1;
-    if(cnt==2):print(f"{i} is Prime Number")
-    else:print(f"{i} is not a prime number")
-    cnt=0;
+    return cnt
+
+num = int(input("Enter an integer"))
+for i in range(num):
+    if(checkPrime(i)==2):
+        print(f"{i} is Prime Number")
+    else:
+        print(f"{i} is not a prime number")
